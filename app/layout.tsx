272df2d3de09feba6aa5c13f6dd5e6c6ecd27e18
ivan2214 <bongiovanniivan12@gmail.com>
2025-01-19 01:35:31 -0300

    feat: integrate vercel analytics
    adds vercel analytics to track website usage.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
 				<Header />
 				<main>{children}</main>
 				<Toaster />
+				<Analytics />
 			</body>
 		</html>
 	);
